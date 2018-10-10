a1c30bc23858ac8ff89d5e1c274bf243f6629ed1
mehdi <coderguy1999@gmail.com>
2018-10-10 09:54:53 +0330

    added audio option and ask before download

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,3 +1,4 @@
+
 from youtube_functions import *
 import pyperclip,os,subprocess
 
@@ -6,10 +7,23 @@ urls=pyperclip.paste()
 valid_urls=get_youtube_urls(urls)
 if valid_urls:
     if args.type.lower() in ['mp4','webm']:
+        if args.ask_for_download:
+            for url in valid_urls:
+                clear_scr()
+                qualityItag=str(qualitiesItags[args.type.lower()][args.quality.lower()])
+                print("Fetching filename for url : {}".format(url[0]))
+                fileName=get_file_name(url[0],qualityItag)
+                print("\n The Video name is : {} ".format(fileName))
+                choice=input("do you want to download this file ? (yes or no) : ")
+                if choice.lower()=="yes":
+                    download_video(url[0].strip(),args.type,args.quality,True,args.output)
+        else:
+            for url in valid_urls:
+                download_video(url[0].strip(),args.type,args.quality,True,args.output)
+        # convert_vtt_to_srt(path)
+    elif args.type.lower() in ['m4a','ogg','mp3']:
         for url in valid_urls:
-            download_video(url[0].strip(),args.type,args.quality,True,args.output)
-        convert_vtt_to_srt(path)
-    elif args.type.lower() in ['mp3','ogg']:
-        for url in valid_urls:
-            download_audio(url[0].strip(),args.type,args.quality,args.output)
-    subprocess.Popen(r"explorer /select,'"+args.output+"'")
+            download_audio(url[0].strip(),args.type,args.output)
+    else:
+        print("error")
+    subprocess.Popen(r'explorer /open,"{}"'.format(args.output))
--- youtube_functions.py
+++ youtube_functions.py
@@ -23,6 +23,7 @@ def initialize_terminal_parser():
     parser.add_argument("-t","--type",help="Type of the file to download (mp4 or webm or 3gp)",default="mp4")
     parser.add_argument("-q","--quality",help="file quality to download",default=None)
     parser.add_argument("-o","--output",help="file path to dowloadfile into it ",default="./")
+    parser.add_argument("-ask","--ask-for-download",help="You want to ask before download ? (1 or 0)",type=int)
     return parser.parse_args()
 
 
@@ -30,9 +31,10 @@ def get_youtube_urls(urls):
     youtubeRegex=re.compile("((http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+)+")
     if youtubeRegex.search(urls):
         return youtubeRegex.findall(urls)
-def get_file_name(url,basePath,quality):
-    fileName=os.popen("youtube-dl "+url+" -f "+quality+" --get-filename --skip-download --restrict-filename -o %(title)s.%(ext)s").read()
-    return os.path.join(basePath,fileName)
+def get_file_name(url,quality):
+    command='youtube-dl "{}"'.format(url)+' -f '+quality+' --get-filename --skip-download --restrict-filename -o %(title)s.%(ext)s'
+    fileName=os.popen(command).read()
+    return fileName
 def get_subtitle_filename(filename,basePath):
     return os.path.join(basePath,os.path.splitext(filename)[0]+".en.srt")
 def generate_new_filename(fileName,basePath):
@@ -40,20 +42,48 @@ def generate_new_filename(fileName,basePath):
 
 def download_video(url,type,quality,subtitle=False,path=False):
     create_path_if_not_exists(path)
-    command="youtube-dl {} --restrict-filename".format(url)
+    command='youtube-dl "{}" --restrict-filename'.format(url)
     qualityItag=None
     if type.lower() in ['mp4','webm']:
         qualityItag=str(qualitiesItags[type.lower()][quality.lower()])
-        command+=" -f "+qualityItag
+        command+=' -f '+qualityItag
     if subtitle:
-        command+=" --write-sub --sub-format srt --sub-lang en --convert-subs srt"
+        command+=' --write-sub --sub-format srt --sub-lang en --convert-subs srt'
     if path:
-        command+=r" -o "+path+"\\%(title)s.%(ext)s"
+        command+=' -o "'+path+'\%(title)s.%(ext)s'+'"'
     clear_scr()
     os.system(command)
-    fileName=get_file_name(url,path,qualityItag)
+    # fileName=get_file_name(url,path,qualityItag)
     # merge_subtitle(fileName,get_subtitle_filename(fileName,path),generate_new_filename(fileName,path))
-# def download_audio(url,type,quality,path):
+def get_latest_m4a_file(path):
+    list_of_files = glob.glob(os.path.join(path,"*.m4a")) # * means all if need specific format then *.csv
+    latest_file = max(list_of_files, key=os.path.getctime)
+    return latest_file
+def get_latest_webm_file(path):
+    list_of_files = glob.glob(os.path.join(path,"*.webm")) # * means all if need specific format then *.csv
+    latest_file = max(list_of_files, key=os.path.getctime)
+    return latest_file
+def convert_m4a_to_mp3(path):
+    m4aFile=get_latest_m4a_file(path)
+    mp3File=m4aFile.replace(".m4a",".mp3")
+    command='ffmpeg -i "{}" -acodec libmp3lame -ab 256k "{}"'.format(m4aFile,mp3File)
+    os.system(command)
+    os.unlink(m4aFile)
+def convert_webm_to_mp3(path):
+    webmFile=get_latest_webm_file(path)
+    mp3File=webmFile.replace(".webm",".mp3")
+    command='ffmpeg -i "{}" -acodec libmp3lame -ab 256k "{}"'.format(m4aFile,webmFile)
+    os.system(command)
+    os.unlink(webmFile)
+def download_audio(url,type,path):
+    command='youtube-dl "'+url+'" -f \'bestaudio[ext='+("m4a" if type=="mp3" else type)+']\' -o "'+path+'\%(title)s.%(ext)s" --no-playlist'
+    if os.system(command) !=0:
+        command='youtube-dl "'+url+'" -f bestaudio -o "'+path+'\%(title)s.%(ext)s" --no-playlist'
+        os.system(command)
+        if type=="mp3":
+            convert_webm_to_mp3(path)
+    elif type=="mp3":
+        convert_m4a_to_mp3(path)
 
 def create_path_if_not_exists(path):
     if not os.path.exists(path):
